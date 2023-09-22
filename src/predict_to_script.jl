d5e6351c0c929b798cffedfc97baa61d3b69c891
moo3030 <mohamedabdelhamid3030@gmail.com>
2023-09-22 19:05:57 +0300

    add converting scripts

---
+++ src/predict_to_script.jl
@@ -0,0 +1,17 @@
+using JSON
+
+function notebook_to_script(ipynb_filename, jl_filename)
+    data = JSON.parsefile(ipynb_filename)
+
+    open(jl_filename, "w") do f
+        for cell in data["cells"]
+            if cell["cell_type"] == "code"
+                println(f, "#= Start of Cell =#")
+                println(f, join(cell["source"], "\n"))
+                println(f, "#= End of Cell =#\n\n")
+            end
+        end
+    end
+end
+
+notebook_to_script("./predict.ipynb", "./predict.jl")
